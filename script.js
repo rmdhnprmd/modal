182794924a67163011c08a1bbd2af324300a3379
rmdhnprmd <ramadhan.permadi36@gmail.com>
2022-07-20 21:53:50 +0700

    tombol close

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ console.log(btnsOpenModal);
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].onclick = () => {
     console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   };
+
+btnCloseModal.onclick = () => {
+  console.log('Close button clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+overlay.onclick = () => {
+  console.log('overlay close clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
