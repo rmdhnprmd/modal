2b4738713e4e9c8c4c8b12d2b37a125f90b0a244
rmdhnprmd <ramadhan.permadi36@gmail.com>
2022-07-20 22:39:09 +0700

    add dry code

--- script.js
+++ script.js
@@ -5,26 +5,18 @@ const overlay = document.querySelector('.overlay');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
 
+const modalOpen = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const modalClose = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].onclick = () => {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  };
+  btnsOpenModal[i].onclick = () => modalOpen();
 
-const modalClose = className => {
-  modal.classList.add(className);
-};
-
-const overlayClose = className => {
-  overlay.classList.add(className);
-};
-
-btnCloseModal.onclick = () => {
-  modalClose('hidden');
-  overlayClose('hidden');
-};
-
-overlay.onclick = () => {
-  modalClose('hidden');
-  overlayClose('hidden');
-};
+btnCloseModal.onclick = () => modalClose();
+overlay.onclick = () => modalClose();
